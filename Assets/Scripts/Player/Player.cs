6ccc3951e3bbaa467c40eee62d3d86e43f46a124
mangom0 <michael0516@naver.com>
2025-10-27 12:22:08 +0900

    PlayerStatus & Status struct

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float movespeed = 5;
-
+    public Status playerStatus;
+    public float levelExp = 0.0f;
     Camera veiwCamera;
     PlayerMove controller;
     void Start()
     {
+        playerStatus.hp = 100.0f;
+        playerStatus.damage = 1.0f;
+        playerStatus.moveSpeed = 5f;
         controller = GetComponent<PlayerMove>();
         veiwCamera = Camera.main;
     }
@@ -18,7 +21,7 @@ public class Player : MonoBehaviour
     {
         //Movement input
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        Vector3 moveVelocity = moveInput.normalized * movespeed;
+        Vector3 moveVelocity = moveInput.normalized * playerStatus.moveSpeed;
         controller.Move(moveVelocity);
 
         //Look input
@@ -29,7 +32,6 @@ public class Player : MonoBehaviour
         if (groundPlane.Raycast(ray, out rayDistance))
         {
             Vector3 point = ray.GetPoint(rayDistance);
-            //Debug.DrawLine(ray.origin, point,Color.red);
             controller.LookAt(point);
         }
     }
