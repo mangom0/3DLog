4f56f06d6a2eb76c36fbfff307cb6cab4d1d2e55
Brew-D <bloodinbrewd@gmail.com>
2025-10-27 12:53:58 +0900

    PlayerAnimation
    플레이어 이동 및 공격 시 애니메이션 추가

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] Animator _animator;
     public Status playerStatus;
     public float levelExp = 0.0f;
     Camera veiwCamera;
@@ -19,11 +20,34 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        MoveInput();
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            _animator.SetBool("isMoving", true);
+        }
+        else
+            _animator.SetBool("isMoving", false);
+        LookInput();
+        if(Input.GetMouseButton(0))
+        {
+            _animator.SetBool("isAttack", true);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            _animator.SetBool("isAttack", false);
+        }
+    }
+
+    void MoveInput()
+    {
         //Movement input
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * playerStatus.moveSpeed;
         controller.Move(moveVelocity);
+    }
 
+    void LookInput()
+    {
         //Look input
         Ray ray = veiwCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
@@ -35,4 +59,5 @@ public class Player : MonoBehaviour
             controller.LookAt(point);
         }
     }
+
 }
