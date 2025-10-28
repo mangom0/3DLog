79e3448ffc1de8f8b5c34cd7b6c2ff29eb528834
Complex-Answer <zstep0101@gmail.com>
2025-10-28 15:46:38 +0900

    [Edit] Box Item Add
    [Edit]
    아이템 박스 생성 및 아이템 드랍 기능 추가
    프리팹에 기능 추가

--- Assets/Scripts/Monster/ItemBox.cs
+++ Assets/Scripts/Monster/ItemBox.cs
@@ -1,18 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Status boxStatus;
+    [SerializeField] GameObject[] boxItem;
+
+    [SerializeField ] Animator boxOpen;
+    bool isBoxOpen = false;
+    int boxIndex;
+
+
+    private void Start()
     {
-        
+        boxStatus.hp = 1;
+        boxIndex = UnityEngine.Random.Range(0, 3);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Skill")
+        {
+            boxStatus.hp -= 1;
+            Debug.Log("박스 열림");
+
+        }
+    }
+
+    private void BoxOpen()
+    {
+        if (boxStatus.hp <= 0)
+        {
+          boxOpen.SetBool("IsHit", true);
+          Destroy(gameObject, 2.5f);
+            if (isBoxOpen != true)
+            {
+                Instantiate(boxItem[boxIndex], gameObject.transform.position, Quaternion.Euler(0, 0, 0));
+                isBoxOpen = true;
+            }
+
+
+        }
+
+    }
+
+
+    private void Update()
+    {
+        BoxOpen();
     }
 }
